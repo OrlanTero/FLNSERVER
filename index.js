e90f024f504b8aed063fb6f886f0c88008efb0ad
jhonorlan <jhonorlantero@gmail.com>
2024-06-18 09:03:03 +0800

    nanaman

--- index.js
+++ index.js
@@ -10,15 +10,18 @@ const socket = require('socket.io'),
 // logger.info('Socket > Listening on port');
 
 
-const __PORT = 3005;
+const __PORT = 5000;
 const app = express();
 const http_server = http.createServer(app);
+const https_server = https.createServer(app);
 
 const io = socket(http_server);
+const httpsio = socket(https_server);
 
 app.use(cors());
 
 http_server.listen(__PORT);
+https_server.listen(__PORT + 1);
 
 function start() {
     io.sockets.on('connection', function (socket) {
@@ -38,8 +41,26 @@ function start() {
             io.emit("system_message", data);
         })
     })
+
+    httpsio.sockets.on('connection', function (socket) {
+        console.log("Connected");
+        socket.on("message", function (data) {
+            console.log(data)
+            io.emit("message", data);
+        })
+
+        socket.on("notification", function (data) {
+            console.log(data)
+            io.emit("notification", data);
+        })
+
+        socket.on("system_message", function (data) {
+            console.log(data)
+            io.emit("system_message", data);
+        })
+    })
 }
 
-console.log('Server is Running1!');
+console.log('Server is Running Install!');
 
 start();
